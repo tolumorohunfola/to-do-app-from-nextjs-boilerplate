7438c2dafec70e3c6b5207a7999b96ef5b4d7b3e
Toluwani Morohunfola <taemorohunfola@outlook.com>
2025-09-29 20:45:52 +0100

    Update since last session

--- app/CalendarView/page.tsx
+++ app/CalendarView/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useState } from "react";
 import Calendar from 'react-calendar';
+import 'devextreme/dist/css/dx.light.css';
+import tasks from '../ToDoList/page.tsx';
 import 'react-calendar/dist/Calendar.css'; // default styling
 
 export default function Page() {
     const [date, setDate] = useState(new Date());
+    const views = ['day', 'week', 'agenda'];
 
     return (
         <div>
@@ -15,6 +18,7 @@ export default function Page() {
             </nav>            
             <h2>Selected Date: {date.toDateString()}</h2>
             <Calendar onChange={setDate} value={date} />
+            <Scheduler views={views} currentView="day"></Scheduler>
         </div>
     )
 }
